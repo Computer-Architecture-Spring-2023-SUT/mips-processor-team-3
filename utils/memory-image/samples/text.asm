709c42e028fcc9f08cdb516f2b5d1f1432a52a93
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2023-05-28 21:09:39 +0330

    Change memoey-image util samples

--- utils/memory-image/samples/text.asm
+++ utils/memory-image/samples/text.asm
@@ -1,7 +1,13 @@
-.globl   main 
-main:   
-   addi $s0, $zero, 15
-   addi $t0, $zero, 16
-   add  $t5, $s0, $t0
+   .globl main
+main:
+    addi $s0, $zero, 1234
+    sw $s0, 1($zero)
+    lw $t0, 1($zero)
+    beq $s0, $t0, next
+    addi $t1, $zero, 12
+next:
+    addi $t2, $zero, 15
+    sw $t2, 5($zero)
+    jr $ra
 
-   jr   $ra     # return to Operating System
+
