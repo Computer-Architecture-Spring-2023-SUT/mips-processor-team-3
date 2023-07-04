5440e37b957a58808ece147d231eadaafa1878d2
MohsenGhasemi <mohsenghasemi8156@gmail.com>
2023-07-04 15:01:47 +0330

    Resolve cache miss in pipline

---
+++ utils/pipline-cache-miss/text.asm
@@ -0,0 +1,23 @@
+.text
+and $zero, $zero, $zero
+addi $s0, $zero, 64
+addi $s1, $zero, 84
+lw $s2, 0($zero)
+addi $s3, $zero, 10
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+addi $s2, $s2, 10
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+sw $s2, 0($zero)
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
