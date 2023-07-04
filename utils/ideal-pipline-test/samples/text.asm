7161e659aae861eb1448d4c321be331514d937b6
MohsenGhasemi <mohsenghasemi8156@gmail.com>
2023-07-04 04:44:43 +0330

    Ideal pipline test

---
+++ utils/ideal-pipline-test/samples/text.asm
@@ -0,0 +1,21 @@
+.text
+
+addi $s0, $zero, 10
+and $t0, $t0, $t0
+and $t1, $t1, $t1
+and $t2, $t2, $t2
+and $t3, $t3, $t3
+and $t4, $t4, $t4
+addi $s1, $zero, 20
+and $t0, $t0, $t0
+and $t1, $t1, $t1
+and $t2, $t2, $t2
+and $t3, $t3, $t3
+and $t4, $t4, $t4
+addi $s2, $zero, 30
+and $t0, $t0, $t0
+and $t1, $t1, $t1
+and $t2, $t2, $t2
+and $t3, $t3, $t3
+and $t4, $t4, $t4
+addi $s3, $zero, 40
