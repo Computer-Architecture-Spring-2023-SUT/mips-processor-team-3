f03b8e52e0a63d34297504cccf71884567aa96ff
MohsenGhasemi <mohsenghasemi8156@gmail.com>
2023-07-04 16:18:08 +0330

    Resolve JR in pipline

---
+++ utils/pipline-jr-stall/text.asm
@@ -0,0 +1,21 @@
+.text
+and $zero, $zero, $zero
+addi $s0, $zero, 52
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+jr $s0
+addi $s1, $zero, 20
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+addi $s2, $zero, 30
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
