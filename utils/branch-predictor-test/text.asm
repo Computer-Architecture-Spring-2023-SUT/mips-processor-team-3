b0f2f3c380b32498977d26ded5f2a93e09317e8d
MohsenGhasemi <mohsenghasemi8156@gmail.com>
2023-07-05 02:48:45 +0330

    Debug and test branch predictor

---
+++ utils/branch-predictor-test/text.asm
@@ -0,0 +1,22 @@
+.text 
+and $zero, $zero, $zero
+addi $s0, $zero, 10
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+loop:
+addi $s0, $s0, -1
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+bne $s0, $zero, loop
+addi $s1, $zero, 20
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
