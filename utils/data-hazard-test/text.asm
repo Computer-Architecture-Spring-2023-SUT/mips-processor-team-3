4b1663aeb2d90f20733a8499d4bb26bb79f5723a
MohsenGhasemi <mohsenghasemi8156@gmail.com>
2023-07-05 12:55:49 +0330

    Add MEM to EX forwariding for Data hazard

---
+++ utils/data-hazard-test/text.asm
@@ -0,0 +1,9 @@
+.text 
+and $zero, $zero, $zero
+addi $s0, $zero, 10
+addi $s1, $s0, 10
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
+and $zero, $zero, $zero
